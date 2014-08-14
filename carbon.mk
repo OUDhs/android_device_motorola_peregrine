683209acba51f55ae4387f26abf4f230df2c188a
somcom3x <somcom3@gmail.com>
2014-08-14 19:27:08 -0400

    Carbonize

--- carbon.mk
+++ carbon.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/peregrine/full_peregrine.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/carbon/config/common_phone.mk)
 
 PRODUCT_RELEASE_NAME := peregrine
-PRODUCT_NAME := cm_peregrine
+PRODUCT_NAME := carbon_peregrine
